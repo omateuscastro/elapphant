22d5ae15eabfac7addd675528f3ca9e430db0180
unknown <mcastro.mateus@gmail.com>
2020-09-16 23:28:35 -0300

    Refresh de Elefante na Home.

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -2,7 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:elapphant/app/models/elephant.dart';
 import 'package:elapphant/app/pages/profile.dart';
+import 'package:elapphant/app/stores/elephant_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,29 +14,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Elephant _el = new Elephant(
-      id: 'Teste',
-      name: 'teste',
-      species: 'teste',
-      sex: 'Female',
-      image: 'https://elephant-api.herokuapp.com/pictures/missing.jpg');
+  ElephantStore _elephantStore;
+
+  @override
+  void initState() {
+    super.initState();
+    _elephantStore = GetIt.instance<ElephantStore>();
+    if (_elephantStore.elephant == null) {
+      _elephantStore.getNewElephant();
+    }
+
+    print(_elephantStore.elephant);
+  }
 
   Future<void> _changeElephant() async {
-    Response response = await Dio().get(
-      'https://elephant-api.herokuapp.com/elephants/random',
-    );
-
-    var teste =
-        (response.data as List).map((e) => Elephant.fromJson(e)).toList();
-
-    setState(() {
-      print(teste[0].name);
-      print(teste[0].sex);
-      print(teste[0].image);
-      if (teste[0].name != null) {
-        _el = teste[0];
-      }
-    });
+    await _elephantStore.getNewElephant();
+    print(_elephantStore.elephant);
   }
 
   @override
@@ -63,106 +59,122 @@ class _HomePageState extends State<HomePage> {
                     bottomLeft: Radius.circular(50),
                     bottomRight: Radius.circular(50))),
           ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ProfilePage(),
-                ),
-              );
-            },
-            child: Column(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(
-                            left: 30.0, right: 30.0, bottom: 100, top: 10),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: CachedNetworkImage(
-                            imageUrl: _el.image,
-                            imageBuilder: (context, imageProvider) => Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: imageProvider,
-                                  fit: BoxFit.fill,
-                                ),
-                              ),
+          Observer(
+              name: 'Elephant',
+              builder: (BuildContext context) {
+                return (_elephantStore.elephant != null)
+                    ? GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => ProfilePage(),
                             ),
-                            errorWidget: (context, url, error) =>
-                                Icon(Icons.error),
-                          ),
-                        ),
-                      ),
-                      Card(
-                        margin: const EdgeInsets.only(
-                            left: 30.0, right: 30.0, top: 400),
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        // margin: const EdgeInsets.symmetric(
-                        //     horizontal: 16, vertical: 12),
-                        child: ListTile(
-                          title: Text(
-                            _el.name,
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              _el.sex == 'Female'
-                                  ? Container(
-                                      width: 24,
-                                      height: 24,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Colors.pink,
-                                      ),
-                                      child: Icon(
-                                        MdiIcons.genderFemale,
-                                        color: Colors.white,
-                                        size: 16,
-                                      ),
-                                    )
-                                  : Container(
-                                      width: 24,
-                                      height: 24,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Colors.blue,
-                                      ),
-                                      child: Icon(
-                                        MdiIcons.genderMale,
-                                        color: Colors.white,
-                                        size: 16,
+                          );
+                        },
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 30.0,
+                                        right: 30.0,
+                                        bottom: 100,
+                                        top: 10),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: CachedNetworkImage(
+                                        imageUrl: _elephantStore.elephant.image,
+                                        imageBuilder:
+                                            (context, imageProvider) =>
+                                                Container(
+                                          decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                              image: imageProvider,
+                                              fit: BoxFit.fill,
+                                            ),
+                                          ),
+                                        ),
+                                        errorWidget: (context, url, error) =>
+                                            Icon(Icons.error),
                                       ),
                                     ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Text(
-                                  _el.species,
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
                                   ),
-                                ),
+                                  Card(
+                                    margin: const EdgeInsets.only(
+                                        left: 30.0, right: 30.0, top: 400),
+                                    elevation: 10,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    // margin: const EdgeInsets.symmetric(
+                                    //     horizontal: 16, vertical: 12),
+                                    child: ListTile(
+                                      title: Text(
+                                        _elephantStore.elephant.name,
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      subtitle: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          _elephantStore.elephant.sex ==
+                                                  'Female'
+                                              ? Container(
+                                                  width: 24,
+                                                  height: 24,
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    color: Colors.pink,
+                                                  ),
+                                                  child: Icon(
+                                                    MdiIcons.genderFemale,
+                                                    color: Colors.white,
+                                                    size: 16,
+                                                  ),
+                                                )
+                                              : Container(
+                                                  width: 24,
+                                                  height: 24,
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    color: Colors.blue,
+                                                  ),
+                                                  child: Icon(
+                                                    MdiIcons.genderMale,
+                                                    color: Colors.white,
+                                                    size: 16,
+                                                  ),
+                                                ),
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(left: 10),
+                                            child: Text(
+                                              _elephantStore.elephant.species,
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w400,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
+                    : Center(
+                        child: CircularProgressIndicator(),
+                      );
+              }),
         ]),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.white,
@@ -173,7 +185,7 @@ class _HomePageState extends State<HomePage> {
             size: 42,
             color: Colors.amber[700],
           ),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
--- lib/app/stores/elephant_store.g.dart
+++ lib/app/stores/elephant_store.g.dart
@@ -9,6 +9,14 @@ part of 'elephant_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$ElephantStore on _ElephantStoreBase, Store {
+  Computed<Elephant> _$elephantComputed;
+
+  @override
+  Elephant get elephant =>
+      (_$elephantComputed ??= Computed<Elephant>(() => super.elephant,
+              name: '_ElephantStoreBase.elephant'))
+          .value;
+
   final _$_elephantAtom = Atom(name: '_ElephantStoreBase._elephant');
 
   @override
@@ -24,10 +32,24 @@ mixin _$ElephantStore on _ElephantStoreBase, Store {
     });
   }
 
+  final _$_ElephantStoreBaseActionController =
+      ActionController(name: '_ElephantStoreBase');
+
+  @override
+  dynamic getNewElephant() {
+    final _$actionInfo = _$_ElephantStoreBaseActionController.startAction(
+        name: '_ElephantStoreBase.getNewElephant');
+    try {
+      return super.getNewElephant();
+    } finally {
+      _$_ElephantStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+elephant: ${elephant}
     ''';
   }
 }
